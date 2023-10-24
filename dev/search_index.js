5a814d1115ccf388db0c71ec0558ae15b99875da
Documenter.jl <documenter@juliadocs.github.io>
2023-10-24 18:02:14 +0000

    build based on 62538a9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ADAPT","category":"page"},{"location":"#ADAPT","page":"Home","title":"ADAPT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ADAPT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ADAPT]","category":"page"}]
+}
