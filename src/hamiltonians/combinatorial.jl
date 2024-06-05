b7b0d11995468b631673f75fddbbc82a1a85329a
yzchen-phy <yanzhucyz@gmail.com>
2024-06-05 19:17:30 -0400

    Add function that extracts MaxCut Hamiltonian input from a graph.

--- src/hamiltonians/combinatorial.jl
+++ src/hamiltonians/combinatorial.jl
@@ -1,9 +1,61 @@
+using Graphs
+using Random
+
 import LinearAlgebra: I
 
 import PauliOperators
 import PauliOperators: FixedPhasePauli, Pauli, ScaledPauli
 import PauliOperators: ScaledPauliVector, PauliSum
 import PauliOperators: clip!, jordan_wigner
+
+_DEFAULT_RNG = MersenneTwister(1234);
+
+"""
+    get_unweighted_maxcut(g::SimpleGraph)
+
+Take a graph object and extract edges for MaxCut.
+
+# Parameters
+- `g`: graph instance.
+
+# Returns
+- `edge_list`: list of edges and weights equal to one.
+
+"""
+function get_unweighted_maxcut(g::SimpleGraph)
+    edge_indices = edges(g)
+    edge_list = Tuple[]
+
+    for e in edge_indices
+        push!(edge_list, (src(e), dst(e), 1.0))
+
+    return edge_list
+end
+
+
+"""
+    get_weighted_maxcut(g::SimpleGraph, rng = _DEFAULT_RNG)
+
+Take a graph object and extract edges and assign edge weights.
+
+# Parameters
+- `g`: graph instance.
+- `rng`: random number generator to generate weights.
+
+# Returns
+- `edge_list`: list of edges and weights.
+
+"""
+function get_weighted_maxcut(g::SimpleGraph, rng = _DEFAULT_RNG)
+    edge_indices = edges(g)
+    edge_list = Tuple[]
+
+    for e in edge_indices
+        push!(edge_list, (src(e), dst(e), rand(rng, Float64)))
+
+    return edge_list
+end
+
 
 """
     maxcut_hamiltonian(V::Int, Edges::Vector{Tuple{Int,Int,T}}) where T<Real
